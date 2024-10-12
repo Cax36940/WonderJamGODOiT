fe018928713b7d1bebb2cdc8c11e4943d26a9d01
corentinleandre <corentinleandre+github@gmail.com>
2024-10-12 18:50:58 -0400

    Adjusted camera movements

--- Scene/Camera3d.cs
+++ Scene/Camera3d.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class Camera3d : Camera3D
 {
-	Vector3 initialPosition = new Vector3(0,4.5f,3.8f) ;
+	Vector3 ReferencePoint = new Vector3(0, 2.6f, 0); 
 	
 	float fovInitial = 100;
 	float minFov = 60;
@@ -11,7 +11,6 @@ public partial class Camera3d : Camera3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		this.Set(Camera3D.PropertyName.Position, initialPosition);
 		this.Set(Camera3D.PropertyName.Fov, fovInitial);
 		
 	}
@@ -19,10 +18,27 @@ public partial class Camera3d : Camera3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		this.Set(Camera3D.PropertyName.Fov, minFov + 0.4f*(100/(MainScene.getInstance().getSpeed()/10)));
+		this.Set(Camera3D.PropertyName.Fov, minFov + 0.4f*(100/(MainScene.getInstance().getSpeed()/8)));
 		
 		
-				
+		Vector3 pos =(Vector3) this.Get(Node3D.PropertyName.Position);		
+		
+		Vector3 diff = pos - ReferencePoint;
+		
+		if(pos.Z <= 11.5){
+			pos.Z += 0.00005f*(MainScene.getInstance().getSpeed());
+		}else if(pos.Y >= 2.6){
+			pos.Y -= 0.00001f*(MainScene.getInstance().getSpeed());
+		}
+		
+			
+		this.Set(Node3D.PropertyName.Position, pos);
+		
+		Vector3 rot = (Vector3) this.Get(Node3D.PropertyName.Rotation);
+		
+		rot.X = (float)Math.Asin(-(diff.Y / diff.Z));
+		
+		this.Set(Node3D.PropertyName.Rotation, rot);
 	}
 	
 	
